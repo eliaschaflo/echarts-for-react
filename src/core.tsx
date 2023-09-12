ef039bd41952bf58fdc4ad3356bcbe6223c292ce
eliaschaflo <30796522+eliaschaflo@users.noreply.github.com>
2023-09-12 14:18:05 +0200

    Update core.tsx

--- src/core.tsx
+++ src/core.tsx
@@ -65,7 +65,7 @@ export default class EChartsReactCore extends PureComponent<EChartsReactProps> {
     }
 
     // when these props are not isEqual, update echarts
-    const pickKeys = ['option', 'notMerge', 'lazyUpdate', 'showLoading', 'loadingOption'];
+    const pickKeys = ['option', 'notMerge', 'replaceMerge', 'lazyUpdate', 'showLoading', 'loadingOption'];
     if (!isEqual(pick(this.props, pickKeys), pick(prevProps, pickKeys))) {
       this.updateEChartsOption();
     }
@@ -183,11 +183,12 @@ export default class EChartsReactCore extends PureComponent<EChartsReactProps> {
    * render the echarts
    */
   private updateEChartsOption(): EChartsInstance {
-    const { option, notMerge = false, lazyUpdate = false, showLoading, loadingOption = null } = this.props;
+    const { option, notMerge = false, replaceMerge, lazyUpdate = false, showLoading, loadingOption = null } = this.props;
+    const opts = { notMerge:notMerge, replaceMerge:replaceMerge, lazyUpdate:lazyUpdate }
     // 1. get or initial the echarts object
     const echartInstance = this.getEchartsInstance();
     // 2. set the echarts option
-    echartInstance.setOption(option, notMerge, lazyUpdate);
+    echartInstance.setOption(option, opts);
     // 3. set loading mask
     if (showLoading) echartInstance.showLoading(loadingOption);
     else echartInstance.hideLoading();
